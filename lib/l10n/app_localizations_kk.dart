eeef3d679b8e7cb0ebc83f276f494d8f5c16a3b3
hypertonika <160002060+hypertonika@users.noreply.github.com>
2025-04-17 17:30:59 +0500

    changes

---
+++ lib/l10n/app_localizations_kk.dart
@@ -0,0 +1,91 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Kazakh (`kk`).
+class AppLocalizationsKk extends AppLocalizations {
+  AppLocalizationsKk([String locale = 'kk']) : super(locale);
+
+  @override
+  String get appTitle => 'Кірістер мен Шығыстар';
+
+  @override
+  String get welcome => 'Қош келдіңіз';
+
+  @override
+  String get addTransaction => 'Транзакция қосу';
+
+  @override
+  String get title => 'Атауы';
+
+  @override
+  String get amount => 'Сома';
+
+  @override
+  String get type => 'Түрі';
+
+  @override
+  String get income => 'Кіріс';
+
+  @override
+  String get expense => 'Шығыс';
+
+  @override
+  String get cancel => 'Бас тарту';
+
+  @override
+  String get add => 'Қосу';
+
+  @override
+  String get pleaseEnterTitle => 'Атауын енгізіңіз';
+
+  @override
+  String get pleaseEnterAmount => 'Соманы енгізіңіз';
+
+  @override
+  String get pleaseEnterValidNumber => 'Дұрыс сан енгізіңіз';
+
+  @override
+  String get noTransactions => 'Транзакциялар жоқ';
+
+  @override
+  String get recentTransactions => 'Соңғы транзакциялар';
+
+  @override
+  String get monthlySummary => 'Айлық есеп';
+
+  @override
+  String get category => 'Санат';
+
+  @override
+  String get amountMustBeGreaterThanZero => 'Сома нөлден үлкен болуы керек';
+
+  @override
+  String get pleaseSelectCategory => 'Санатты таңдаңыз';
+
+  @override
+  String get invalidAmountFormat => 'Соманың форматы дұрыс емес';
+
+  @override
+  String get settings => 'Параметрлер';
+
+  @override
+  String get theme => 'Тақырып';
+
+  @override
+  String get language => 'Тіл';
+
+  @override
+  String get systemDefault => 'Жүйелік';
+
+  @override
+  String get selectLanguage => 'Тілді таңдаңыз';
+
+  @override
+  String get darkTheme => 'Қараңғы тақырып';
+
+  @override
+  String get lightTheme => 'Жарық тақырып';
+}
